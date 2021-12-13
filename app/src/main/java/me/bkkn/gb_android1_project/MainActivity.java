3dcaccc7a82f310aee06249b89552ae7fad118f0
bkkn <bakaikin@yandex.ru>
2021-12-13 11:49:09 +0300

    Initial commit. Provided gitignore used, all files added to vcs

---
+++ app/src/main/java/me/bkkn/gb_android1_project/MainActivity.java
@@ -0,0 +1,13 @@
+package me.bkkn.gb_android1_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
