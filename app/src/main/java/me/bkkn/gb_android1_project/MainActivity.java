2084ed25a6d16ab9345f2068c0ddab9be55b29ae
bkkn <bakaikin@yandex.ru>
2021-12-14 00:52:25 +0300

    Lesson 1 homework seems to be completed. But why Activity fields : name, age are empty after they were set..

--- app/src/main/java/me/bkkn/gb_android1_project/MainActivity.java
+++ app/src/main/java/me/bkkn/gb_android1_project/MainActivity.java
@@ -1,10 +1,16 @@
 package me.bkkn.gb_android1_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +19,13 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextAge;
     EditText editTextPhone;
     EditText editTextEmail;
-    EditText editTextPassword;
+    //    EditText editTextPassword;
     TextView textViewMessage;
-
+    CalendarView calendarView;
+    String name = "";
+    String age = "";
+    String message = "";
+    String selectedDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +35,33 @@ public class MainActivity extends AppCompatActivity {
         attachListeners();
     }
 
-    private void initUI(){
+    private void initUI() {
         button = findViewById(R.id.button_ok);
         editTextName = findViewById(R.id.edit_text_name);
         textViewMessage = findViewById(R.id.edit_text_message);
         editTextAge = findViewById(R.id.edit_text_age);
         editTextPhone = findViewById(R.id.edit_text_phone);
         editTextEmail = findViewById(R.id.edit_text_email);
-        editTextPassword = findViewById(R.id.edit_text_password);
+//        editTextPassword = findViewById(R.id.edit_text_password);
+        calendarView = findViewById(R.id.calendar_view);
     }
 
-    private void attachListeners(){
-        button.setOnClickListener((val)->{
-
-            int maxEms = editTextName.getMaxEms();
-            int minEms = editTextName.getMinEms();
-
-            textViewMessage.setText(editTextName.getText());
+    private void attachListeners() {
+        calendarView.setOnDateChangeListener((calendarView1, i, i1, i2) -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+            selectedDate = sdf.format(new Date(calendarView.getDate()));
         });
+        editTextName.setOnClickListener(view -> name = view.getTransitionName());
+        button.setOnClickListener((view) -> {
+            if (!name.isEmpty() && !age.isEmpty() && !selectedDate.isEmpty())
+                textViewMessage.setText(formMessage());
+            else
+                textViewMessage.setText(R.string.warning);
+        });
+    }
+
+    private String formMessage() {
+        message = "Today is " + selectedDate + " and the person named " + name + " is " + age + " years old";
+        return message;
     }
 }
